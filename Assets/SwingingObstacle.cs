e4e79556714d7d1a14dadc1608c91edc82255fd6
AmyE123 <amy.elliott2002@yahoo.co.uk>
2024-02-17 00:28:26 +0000

    Made swinging obstacle and replaced the player ball with harry's new one

---
+++ Assets/SwingingObstacle.cs
@@ -0,0 +1,32 @@
+namespace CT6RIGPR
+{
+    using UnityEngine;
+
+    /// <summary>
+    /// The class for the swinging obstacle
+    /// </summary>
+    public class SwingingObstacle : MonoBehaviour
+    {
+        [SerializeField] private Rigidbody _rigidbody;
+        [SerializeField] private float _swingForce = 10f;
+        [SerializeField] private Vector3 _swingAxis = Vector3.forward;
+        private Vector3 _initialDirection = Vector3.right;
+
+        private void Start()
+        {
+            _rigidbody.AddForce(_initialDirection * _swingForce, ForceMode.Impulse);
+        }
+
+        private void FixedUpdate()
+        {
+            ApplyContinuousForce();
+        }
+
+        private void ApplyContinuousForce()
+        {
+            Vector3 projectedVelocity = Vector3.ProjectOnPlane(_rigidbody.velocity, _swingAxis);
+            Vector3 swingDirection = Vector3.Cross(_swingAxis, projectedVelocity).normalized;
+            _rigidbody.AddForce(swingDirection * _swingForce * Time.fixedDeltaTime, ForceMode.Force);
+        }
+    }
+}
