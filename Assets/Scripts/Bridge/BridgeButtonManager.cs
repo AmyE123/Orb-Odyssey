add3d49ffd37f147aa30afc7c63c3b98f519df3e
Connor Beach <up2084284@myport.ac.uk>
2024-02-21 18:50:30 +0000

    Added lights to the bridge puzzle

---
+++ Assets/Scripts/Bridge/BridgeButtonManager.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BridgeButtonManager : MonoBehaviour
+{
+    public Renderer[] light0, light1, light2, light3; 
+}
