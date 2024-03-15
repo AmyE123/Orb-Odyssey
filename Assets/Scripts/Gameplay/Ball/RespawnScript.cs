9b73f0f516e2dc6c97795e7c5b1354b1e8ffc649
Layla McCahon <lsmccahon@gmail.com>
2024-03-15 13:45:03 +0000

    Updated respawn script to use material

--- Assets/Scripts/Gameplay/Ball/RespawnScript.cs
+++ Assets/Scripts/Gameplay/Ball/RespawnScript.cs
@@ -1,5 +1,6 @@
 namespace CT6RIGPR
 {
+    using DG.Tweening;
     using System.Collections;
     using UnityEngine;
     using UnityEngine.UI;
@@ -9,65 +10,32 @@ namespace CT6RIGPR
     /// </summary>
     public class RespawnScript : MonoBehaviour
     {
-		private BallController _ballController;
+        private BallController _ballController;
         private bool isRespawning = false;
 
         [SerializeField] private GameObject[] _checkpoints;
-		[SerializeField] private GameManager _gameManager;
-		[SerializeField] private Image _fadeImage;
+        [SerializeField] private GameManager _gameManager;
         [SerializeField] private float _fadeInTime = 0.4f; //Percentage of time spent fading in
         [SerializeField] private float _fadeWaitTime = 0.2f; //Percentage of time waiting.
         [SerializeField] private float _fadeOutTime = 0.4f;  //Percentage of time fading out.
         [SerializeField] private float _respawnTime = Constants.RESPAWN_TIME;
+        [SerializeField] private GlobalGameReferences _globalReferences;
 
         private void Start()
         {
             if (_gameManager == null)
-			{
-				_gameManager = FindObjectOfType<GameManager>();
-				Debug.LogWarning("[CT6RIGPR] GameManager reference in respawn script not set. Please set this in the inspector.");
-			}
-			_ballController = _gameManager.GlobalReferences.BallController;
-
-		}
-
-        private IEnumerator FadeOut()
-        {
-            Color colour = Color.black;
-            float fadeAmount = 0.0f;
-            while (fadeAmount <= 1.0f)
             {
-                //reduce fade amount
-                fadeAmount += Time.deltaTime / (_respawnTime * _fadeOutTime);
-                colour = new Color(colour.r, colour.g, colour.b, fadeAmount);
-				//set that colour
-				_fadeImage.color = colour;
-                //wait for the frame to end
-                yield return null;
+                _gameManager = FindObjectOfType<GameManager>();
+                Debug.LogWarning("[CT6RIGPR] GameManager reference in respawn script not set. Please set this in the inspector.");
             }
-        }
-
-        private IEnumerator FadeIn()
-        {
-            Color colour = Color.black;
-            float fadeAmount = 1.0f;
-            while (fadeAmount >= 0.0f)
-            {
-                //reduce fade amount
-                fadeAmount -= Time.deltaTime / (_respawnTime * _fadeInTime);
-                colour = new Color(colour.r, colour.g, colour.b, fadeAmount);
-				//set that colour
-				_fadeImage.color = colour;
-                //wait for the frame to end
-                yield return null;
-            }
+            _ballController = _gameManager.GlobalReferences.BallController;
+            _checkpoints = _gameManager.GlobalReferences.Checkpoints;
         }
 
         private Vector3 FindCheckpoint()
         {
             GameObject closestCheckpoint = null;
             float distanceSqrToPoint = 100000.0f; //High value that will be beaten by the first checkpoint.
-            _checkpoints = _gameManager.GlobalReferences.Checkpoints;
 
             if (_checkpoints.Length > 0)
             {
@@ -94,13 +62,22 @@ namespace CT6RIGPR
             if (isRespawning)
                 yield break;
 
+            Material ballMat = _globalReferences.BallMaterial;
+
+            if (ballMat == null)
+            {
+                Debug.LogWarning("[CT6RIGPR]: Material to fade is not assigned.");
+                yield break;
+            }
+            ballMat.color = Color.clear;
+
             isRespawning = true;
 
             _gameManager.GlobalReferences.GameSFXManager.PlayOutOfBoundsNegativeSound();
 
             // Disable movement
             _ballController.DisableInput();
-            StartCoroutine(FadeOut());
+            ballMat.DOColor(Color.black, 1).SetDelay(_fadeOutTime);
 
             yield return new WaitForSeconds(_respawnTime * (_fadeOutTime + _fadeWaitTime / 2));
 
@@ -110,10 +87,8 @@ namespace CT6RIGPR
 
             yield return new WaitForSeconds(_respawnTime * (_fadeWaitTime / 2));
 
-            StartCoroutine(FadeIn());
+            ballMat.DOColor(Color.clear, 1).SetDelay(_fadeInTime);
             yield return new WaitForSeconds(_respawnTime * _fadeInTime);
-
-            _fadeImage.color = new Color(0.0f, 0.0f, 0.0f, 0.0f);
 
             // Re-enable movement
             _ballController.EnableInput();
@@ -131,7 +106,7 @@ namespace CT6RIGPR
 
         private void OnTriggerEnter(Collider other)
         {
-            if(other.tag == Constants.WATER_TAG)
+            if (other.tag == Constants.WATER_TAG)
             {
                 Debug.Log("WATER!");
                 StartCoroutine(Respawn());
