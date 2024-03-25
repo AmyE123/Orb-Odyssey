8a216e2a59300d2055b8ed106732ff4ce53eaf90
ConnorJBeach <up2084284@myport.ac.uk>
2024-03-25 13:03:24 +0000

    Changes to get things hopefully working

--- Assets/ThirdParty/TerrainFormer/Settings.tf
+++ Assets/ThirdParty/TerrainFormer/Settings.tf
@@ -332,7 +332,7 @@
     "generateRampCurveInXAxis": true,
     "generateHeight": 5.0,
     "flattenMode": 0,
-    "setHeight": 3.167917251586914,
+    "setHeight": 12.485445022583008,
     "boxFilterSize": 1,
     "smoothingIterations": 1,
     "mouldToolRaycastOffset": 0.0,
